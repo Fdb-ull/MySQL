d2defd71ff967f7281faf0e2768fe8fbf31496a3
Franco <alu0101134703@ull.edu.es>
2024-07-10 22:29:20 +0100

    Ejemplos SELECT

---
+++ practica/Statements/select.sql
@@ -0,0 +1,6 @@
+# Devuelve todos los campos de la tabla users
+SELECT * FROM users;
+# Devuelve el campo user_id de la tabla users
+SELECT user_id FROM users;
+# Devuelve los campos user_id y email de la tabla users
+SELECT user_id, email FROM users;
